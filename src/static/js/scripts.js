aaab5d112a7ba7d6b8da269ee6848ea3d6a05f9e
imaneid77 <149710449+imaneid77@users.noreply.github.com>
2024-05-28 10:16:02 +0200

    affichage de l'image en plus de sa classification

--- src/static/js/scripts.js
+++ src/static/js/scripts.js
@@ -4,16 +4,25 @@ function uploadImage() {
     const formData = new FormData();
     formData.append('file', file);
 
+    // Afficher l'image sélectionnée
+    const reader = new FileReader();
+    reader.onload = function(e) {
+        const img = document.getElementById('uploadedImage');
+        img.src = e.target.result;
+        img.style.display = 'block';
+    }
+    reader.readAsDataURL(file);
+
+    // Envoyer l'image au serveur pour la classification
     fetch('/predict', {
         method: 'POST',
         body: formData
     })
         .then(response => response.json())
         .then(data => {
-            const resultSection = document.getElementById('resultSection');
             const resultText = document.getElementById('resultText');
             resultText.innerText = data.class;
-            resultSection.style.display = 'block';
+            document.getElementById('resultSection').style.display = 'block';
         })
         .catch(error => console.error('Error:', error));
 }
